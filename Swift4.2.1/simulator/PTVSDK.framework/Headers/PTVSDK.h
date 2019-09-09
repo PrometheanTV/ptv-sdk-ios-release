33f262b2af025810f178d9ff0dd06fc6ababf5fb
Andi Wilson <andi.wilson@promethean.tv>
2019-09-08 17:16:01 -0700

    Binaries for v0.7.0

---
+++ Swift4.2.1/simulator/PTVSDK.framework/Headers/PTVSDK.h
@@ -0,0 +1,17 @@
+//
+//  PTVSDK.h
+//  PTVSDK
+//
+//  Created by Andi Wilson on 2/26/18.
+//  Copyright © 2018 Promethean TV, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PTVSDK.
+FOUNDATION_EXPORT double PTVSDKVersionNumber;
+
+//! Project version string for PTVSDK.
+FOUNDATION_EXPORT const unsigned char PTVSDKVersionString[];
+
+// In this header, we should import all the public headers of our framework using statements like #import <PTVSDK/PublicHeader.h>
